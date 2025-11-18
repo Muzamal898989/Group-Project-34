6dda11479fd4a95282ee3c8a483760541dc18fa5
Genie231 <240111450@aston.ac.uk>
2025-11-18 17:03:00 +0000

    Add files via upload

---
+++ Group-Project-34/java.js
@@ -0,0 +1,16 @@
+ 
+    document.getElementById("loginForm").addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      const username = document.getElementById("username").value;
+      const password = document.getElementById("password").value;
+
+      if (username === "admin" && password === "1234") {
+        alert("Login successful!");
+        window.location.href = "dashboard.html";
+      } else {
+        document.getElementById("message").textContent =
+          "Invalid username or password.";
+      }
+    });
+  
